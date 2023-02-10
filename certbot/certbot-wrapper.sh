5c7088f1ca18620649ba9a9b24e5a08c95664ae4
M0Rf30 <morf3089@gmail.com>
2023-02-10 14:59:28 +0100

    feat: switch to pyinstaller approach

--- certbot/certbot-wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONPATH="/opt/zextras/common/lib/python3.8/site-packages/:/opt/zextras/common/lib64/python3.8/site-packages/${PYTHONPATH}"
-/opt/zextras/common/bin/carbonio-certbot "${@}"
