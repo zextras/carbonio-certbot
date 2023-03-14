446e27f79bc55b455157b130e33ddcfd5b2810b6
M0Rf30 <morf3089@gmail.com>
2023-03-14 12:44:17 +0100

    chore: use specific pythonpath for certbot

--- certbot/certbot-wrapper.sh
+++ certbot/certbot-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PYTHONPATH="/opt/zextras/common/lib/python3.8/site-packages/:/opt/zextras/common/lib64/python3.8/site-packages/${PYTHONPATH}"
-/opt/zextras/common/bin/carbonio-certbot "${@}"
+export PYTHONPATH="/opt/zextras/common/certbot/lib/python3.8/site-packages/:/opt/zextras/common/certbot/lib64/python3.8/site-packages/${PYTHONPATH}"
+/opt/zextras/common/certbot/bin/carbonio-certbot "${@}"
