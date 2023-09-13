9b8280df061a10cbdbb0ec1cb64a6e7f93386e23
M0Rf30 <morf3089@gmail.com>
2023-09-13 16:55:07 +0200

    feat: add ubuntu 22.04 (jammy jellyfish) support
    refs: IN-610

--- certbot/certbot-wrapper.sh
+++ certbot/certbot-wrapper.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-export PYTHONPATH="/opt/zextras/common/certbot/lib/python3.8/site-packages/:/opt/zextras/common/certbot/lib64/python3.8/site-packages/${PYTHONPATH}"
+
+# retrieve python3 version (major.minor)
+py_ver=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
+
+# set proper modules path
+PYTHONPATH="/opt/zextras/common/certbot/lib/${py_ver}/dist-packages:"
+PYTHONPATH+="/opt/zextras/common/certbot/lib/${py_ver}/site-packages:"
+PYTHONPATH+="/opt/zextras/common/certbot/lib64/${py_ver}/dist-packages:"
+PYTHONPATH+="/opt/zextras/common/certbot/lib64/${py_ver}/site-packages:"
+export PYTHONPATH
+
+# main certbot entrypoint with args
 /opt/zextras/common/certbot/bin/carbonio-certbot "${@}"
